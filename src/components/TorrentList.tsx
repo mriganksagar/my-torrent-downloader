b8e34b947e96acd3f1b66da839d208a1807ec9c4
mrig <mriganksagar@gmail.com>
2024-02-14 20:12:01 +0530

    minor change

--- src/components/TorrentList.tsx
+++ src/components/TorrentList.tsx
@@ -7,13 +7,31 @@ import { EmptyContent } from "./EmptyContent";
 import { cn } from "@/lib/utils";
 
 export function TorrentList() {
-  useRefresher();
-  const torrentInfoList = webTorrentClient.torrents.map(torrent => new TorrentInfo(torrent));
-  
-  return (
-    <>
-    <h1 className={cn("text-3xl", "font-bold", "text-center","mt-4", "text-blue-950")}>Your Added Torrents</h1>
-      {torrentInfoList.length ? <DataTable columns={TorrentListColumns} data={torrentInfoList} /> : <EmptyContent />}
-    </>
-  );
+	useRefresher();
+	const torrentInfoList = webTorrentClient.torrents.map(
+		(torrent) => new TorrentInfo(torrent),
+	);
+
+	return (
+		<>
+			{torrentInfoList.length ? (
+				<>
+					<h1
+						className={cn(
+							"text-3xl",
+							"font-bold",
+							"text-center",
+							"mt-4",
+							"text-blue-950",
+						)}
+					>
+						Your Added Torrents
+					</h1>
+					<DataTable columns={TorrentListColumns} data={torrentInfoList} />
+				</>
+			) : (
+				<EmptyContent />
+			)}
+		</>
+	);
 }
