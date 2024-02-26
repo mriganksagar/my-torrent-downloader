11117af6eae220a793d484ca901827e223789885
mrig <mriganksagar@gmail.com>
2024-02-26 19:21:31 +0530

    ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import {fileURLToPath} from 'url'
 import path from 'path'
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [basicSsl({name: "torrentdownloader"}), svgr(), react()],
   resolve: {
     alias: {
       'webtorrent': fileURLToPath(new URL('./node_modules/webtorrent/dist/webtorrent.min.js', import.meta.url)),
@@ -13,4 +14,3 @@ export default defineConfig({
     },
   },
 })
-
